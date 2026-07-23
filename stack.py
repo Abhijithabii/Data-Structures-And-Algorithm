11a29582548a3df409447bd95aec9837bb31df26
Abhijithabii <abhijithrajeevan123@gmail.com>
2026-07-23 19:27:03 +0530

    created new module for learning stack and initiated stack with its common functionalities

---
+++ stack.py
@@ -0,0 +1,17 @@
+class StackList:
+    def __init__(self):
+        self.items = []
+
+    def push(self,data):
+        pass
+    def pop(self):
+        pass
+    def peek(self):
+        pass
+    def is_empty(self):
+        pass
+    def size(self):
+        pass
+    def clear(self):
+        """Remove all elements."""
+        pass
