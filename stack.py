1657bb070e1038b9263b0f42cfbd5863840edeee
Abhijithabii <abhijithrajeevan123@gmail.com>
2026-07-23 19:36:40 +0530

    stack implimentation with list basic operations are completed

--- stack.py
+++ stack.py
@@ -3,15 +3,37 @@ class StackList:
         self.items = []
 
     def push(self,data):
-        pass
+        self.items.append(data)
+        return
+    
     def pop(self):
-        pass
+        if self.is_empty():
+            return IndexError("Stack is empty")
+        self.items.pop()
     def peek(self):
-        pass
+        if self.is_empty():
+            return IndexError("Stack is empty")
+        return self.items[-1]
+    
     def is_empty(self):
-        pass
+        return len(self.items) == 0
+    
     def size(self):
-        pass
+        return len(self.items)
+    
     def clear(self):
         """Remove all elements."""
-        pass
+        return self.items.clear()
+
+    def show_stack(self):
+        return self.items
+
+
+stack1= StackList()
+stack1.push(1)
+stack1.push(2)
+stack1.push(3)
+print(stack1.peek())
+print(stack1.show_stack())
+stack1.pop()
+print(stack1.show_stack())
