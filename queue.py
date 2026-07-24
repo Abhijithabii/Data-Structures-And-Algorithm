cf39701f5a56809aef2ed087588acf558e1443ea
Abhijithabii <abhijithrajeevan123@gmail.com>
2026-07-24 06:36:21 +0530

    created new module for the queue learning with almost all the functioality derived in the module

---
+++ queue.py
@@ -0,0 +1,34 @@
+class QueueList:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, data):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def show_queues(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
